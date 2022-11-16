c9f94565d247dbb19f92c6ee9bda67b5f85c281e
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-11-16 22:40:34 +0000

    new vars

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,12 +1,9 @@
 import NextAuth from "next-auth";
-import nodemailer from "nodemailer";
-import CredentialsProvider from "next-auth/providers/credentials";
-import EmailProvider from "next-auth/providers/email";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { verifyPassword } from "../../../lib/auth";
+
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 function html({ url, host, email }) {
@@ -32,34 +29,6 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   // Configure one or more authentication providers
   providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-      // async sendVerificationRequest({
-      //   identifier: email,
-      //   url,
-      //   provider: { server, from },
-      // }) {
-      //   const { host } = new URL(url);
-      //   const transport = nodemailer.createTransport(server);
-
-      //   await transport.sendMail({
-      //     to: email,
-      //     from,
-      //     subject: `Sign in to ${host}`,
-      //     text: text({ url, host }),
-      //     html: html({ url, host, email }),
-      //   });
-      //   console.log("AND HERE");
-      // },
-    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,

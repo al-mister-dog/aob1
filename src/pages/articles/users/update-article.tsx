befde346de92e199448b67a17e8eb085c25e6d02
alex hunter <alexhunter@alexs-MacBook-Air.local>
2022-11-29 22:18:53 +0000

    log an error

--- src/pages/articles/users/update-article.tsx
+++ src/pages/articles/users/update-article.tsx
@@ -2,7 +2,11 @@ import { Box } from "@mantine/core";
 import { getSession } from "next-auth/react";
 import ArticleForm from "../../../components/desktop/profile/articles/form-update";
 import { colors } from "../../../config/colorPalette";
-export default function NewArticle({ article, user }) {
+export default function NewArticle(props) {
+  const { article, user, error } = props;
+  if (error) {
+    alert(JSON.stringify(error));
+  }
   return (
     <Box style={{ minHeight: "100vh" }}>
       <Box
@@ -19,33 +23,39 @@ export default function NewArticle({ article, user }) {
   );
 }
 export async function getServerSideProps(context) {
-  const session = await getSession(context);
+  try {
+    const session = await getSession(context);
 
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/registration/signin",
+    if (!session) {
+      return {
+        redirect: {
+          destination: "/registration/signin",
+        },
+      };
+    }
+
+    const data = await prisma.post.findUnique({
+      where: { id: context.query.articleId },
+      include: {
+        user: true,
       },
+    });
+
+    const article = {
+      id: data.id,
+      title: data.title,
+      preview: data.preview,
+      body: data.body,
+      path: data.path,
+      userId: data.userId,
+    };
+    const user = data.user;
+    return {
+      props: { article, user },
+    };
+  } catch (error) {
+    return {
+      props: { error },
     };
   }
-
-  const data = await prisma.post.findUnique({
-    where: { id: context.query.articleId },
-    include: {
-      user: true,
-    },
-  });
-
-  const article = {
-    id: data.id,
-    title: data.title,
-    preview: data.preview,
-    body: data.body,
-    path: data.path,
-    userId: data.userId,
-  };
-  const user = data.user;
-  return {
-    props: { article, user },
-  };
 }
